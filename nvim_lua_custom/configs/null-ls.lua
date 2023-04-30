d9917d8d92aeee13b0c44ee50e472d3924597b9c
Nathan Hüsken <nathan@wintercloud.de>
2023-04-30 22:01:41 +0200

    setup leptos-language-server

--- nvim_lua_custom/configs/null-ls.lua
+++ nvim_lua_custom/configs/null-ls.lua
@@ -1,10 +1,22 @@
 -- custom/configs/null-ls.lua
 
 local null_ls = require "null-ls"
+local h = require("null-ls.helpers")
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
+
+-- leptosfmt source
+local leptos_fmt_source = h.make_builtin({
+  filetypes = {"rust"},
+  method = require("null-ls.methods").internal.FORMATTING,
+  factory = h.formatter_factory,
+  generator_opts = {
+    command = "leptosfmt",
+    to_temp_file = true
+  }
+})
 
 local sources = {
   formatting.prettier,
@@ -17,6 +29,7 @@ local sources = {
   lint.actionlint,
   lint.ansiblelint,
   lint.chktex,
+  leptos_fmt_source,
   code_actions.gitsigns.with({
     config = {
         filter_actions = function(title)
@@ -26,6 +39,8 @@ local sources = {
   })
   --  code_actions.gitsigns
 }
+
+
 
 null_ls.setup {
    debug = true,
--- nvim_lua_custom/init.lua
+++ nvim_lua_custom/init.lua
@@ -2,3 +2,15 @@ local set = vim.opt
 
 set.spelllang = 'en,de,cjk'
 set.guifont = 'JetBrainsMono NF:h14'
+
+-- leptos cusotm lsp
+vim.api.nvim_create_autocmd({"BufRead"},{
+  pattern = {"*.rs"},
+  callback = function()
+    vim.lsp.start({
+      name = "leptos-language-server",
+      cmd = {"leptos-language-server"},
+      root_dir = vim.fs.dirname(vim.fs.find({'Cargo.toml'}, { upward = true })[1]),
+    });
+  end
+})
