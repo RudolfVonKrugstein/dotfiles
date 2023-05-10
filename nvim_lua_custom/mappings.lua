5fdb6b87817b7ebc457aa264f1824aafde12dd55
Nathan Hüsken <nathan@wintercloud.de>
2023-05-10 08:34:10 +0200

    adjust keybindings a bit to be like intellij for debugging

--- nvim_lua_custom/mappings.lua
+++ nvim_lua_custom/mappings.lua
@@ -2,16 +2,16 @@ local M = {}
 
 M.dap = {
   n = {
-    ["<leader>db"] = {":lua require'dap'.toggle_breakpoint()<CR>", "Toggle Breakpoint"},
-    ["<leader>dr"] = {":lua require'dap'.run_last()<CR>", "Debug run (last)"},
-    ["<leader>dR"] = {":lua require'dap'.restart()<CR>", "Debug restart"},
+    ["<C-F8>"] = {":lua require'dap'.toggle_breakpoint()<CR>", "Toggle Breakpoint"},
+    ["<S-F9>"] = {":lua require'dap'.run_last()<CR>", "Debug run (last)"},
+    ["<C-F9>"] = {":lua require'dap'.restart()<CR>", "Debug restart"},
     ["<leader>ds"] = {":lua require'dap'.terminate()<CR>", "Debug stop/terminate"},
-    ["<leader>dc"] = {":lua require'dap'.continue()<CR>", "Debug continue"},
+    ["<F9>"] = {":lua require'dap'.continue()<CR>", "Debug continue"},
     ["<leader>du"] = {":lua require'dapui'.toggle()<CR>", "Debug toggle ui"},
     ["<leader>de"] = {":lua require'dapui'.eval()<CR>", "Debug evaluate expression under curser"},
-    ["<leader>so"] = {":lua require'dap'.step_over()<CR>", "Debug step over"},
-    ["<leader>si"] = {":lua require'dap'.step_into()<CR>", "Debug step into"},
-    ["<leader>st"] = {":lua require'dap'.step_out()<CR>", "Debug step out"},
+    ["<F8>"] = {":lua require'dap'.step_over()<CR>", "Debug step over"},
+    ["<F7>"] = {":lua require'dap'.step_into()<CR>", "Debug step into"},
+    ["<S-F8>"] = {":lua require'dap'.step_out()<CR>", "Debug step out"},
 
     ["<leader>tt"] = {":lua require'trouble'.open({mode=\"workspace_diagnostics\"})<CR>"},
     ["<C-l>"] = {":lua require(\"tmux\").move_right()<CR>"},
