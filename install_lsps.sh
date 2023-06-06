87c0c988870f91b610ee4d4cab09c206d7451e95
Nathan Hüsken <nathan@wintercloud.de>
2023-06-06 08:47:52 +0200

    configure markdown for helix

--- install_lsps.sh
+++ install_lsps.sh
@@ -2,3 +2,5 @@ pip install python-lsp-server[all]
 pip install flake8
 pip install black
 pip install ruff
+pip install mdformat mdformat-black
+wget https://github.com/artempyanykh/marksman/releases/download/2023-06-05/marksman-linux-x64
