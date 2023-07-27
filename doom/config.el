6ac656bd475ecc71e170c6abc12ed96d40e0b7f9
Nathan Hüsken <nathan@wintercloud.de>
2023-07-27 11:27:27 +0200

    put org path into projects path

--- doom/config.el
+++ doom/config.el
@@ -47,7 +47,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/projects/org/")
 
  (defun center-cargo-test-error ()
       "Center the error in the error window"
