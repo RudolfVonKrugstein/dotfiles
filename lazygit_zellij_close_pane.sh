ba7378ae1689cd228f6af5eb81ae07795db74af1
Nathan Hüsken <nathan@wintercloud.de>
2023-09-23 07:50:33 +0200

    extend path before starting lazygit

--- lazygit_zellij_close_pane.sh
+++ lazygit_zellij_close_pane.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [  -d "$HOME/go/bin" ]; then
+        export PATH="$PATH:$HOME/go/bin"
+fi
+if [  -d "$HOME/.cargo/bin" ]; then
+        export PATH="$PATH:$HOME/.cargo/bin"
+fi
 lazygit
 zellij action close-pane
