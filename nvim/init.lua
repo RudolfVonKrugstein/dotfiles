db12b0f18921df82134e36430452e8271072ddfb
Nathan Hüsken <nathan.huesken-extern@deutschebahn.com>
2024-02-05 13:52:43 +0100

    add treesitter textobjects

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
   { "nvim-lualine/lualine.nvim" },
   { "mhartington/formatter.nvim" },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
   { "nvim-telescope/telescope.nvim" },
   { "nvim-telescope/telescope-ui-select.nvim" },
   { "nvim-telescope/telescope-file-browser.nvim" },
@@ -173,6 +174,76 @@ lsp_zero.set_sign_icons({
   warn = "▲",
   hint = "⚑",
   info = "»",
+})
+
+-- setup treesitter
+require("nvim-treesitter.configs").setup({
+  -- Add languages to be installed here that you want installed for treesitter
+  ensure_installed = "all",
+
+  -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+  auto_install = true,
+  -- Install languages synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+  -- List of parsers to ignore installing
+  ignore_install = {},
+  -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+  modules = {},
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<c-space>",
+      node_incremental = "<c-space>",
+      scope_incremental = "<c-s>",
+      node_decremental = "<M-space>",
+    },
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+  },
 })
 
 -- setup mason and lspconfig
