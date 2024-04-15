803653749d193a17e4a5b41562d40d1f7d9eb890
Nathan Hüsken <nathan.huesken-extern@deutschebahn.com>
2024-04-15 14:50:21 +0200

    use worktrees in zsp script

--- zsp.sh
+++ zsp.sh
@@ -13,6 +13,33 @@ echo "OK, session $session_name for $dest_dir"
 # goto directory
 cd "$dest_dir"
 
-# rename the tab
-zellij action rename-tab "$session_name"
+# list branches
+worktree_branch=$(git branch -a --format "%(refname)" \
+  | sed 's|^.*/\(.*\)$|\1|g' \
+  | sort \
+  | uniq \
+  | (echo "no worktree"; echo "new branch"; cat) \
+  | fzf --prompt "worktree: " \
+    --bind 'tab:up' \
+    --bind 'shift-tab:down')
 
+if [[ "$worktree_branch" != "no worktree" ]]; then
+  if [[ "$worktree_branch" == "new branch" ]]; then
+    read -p "Branch Name: " worktree_branch
+    git branch "$worktree_branch"
+  fi
+
+  if git worktree list | grep "\[$worktree_branch\]"; then
+    worktree_branch_dir=$(git worktree list | grep "\[$worktree_branch\]" | awk '{print $1}')
+  else
+    worktree_branch_dir=$(echo "$worktree_branch" | tr "/" "_")
+    git worktree add "$HOME/worktrees/$session_name/$worktree_branch_dir" 
+  fi
+  echo "Switching to $worktree_branch_dir"
+  cd $worktree_branch_dir
+fi
+
+# rename the tab
+zellij action rename-tab "$session_name>$worktree_branch"
+
+# list the branches
