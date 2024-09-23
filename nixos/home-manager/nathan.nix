3ba37a7f61b8760efebf25c278aeb542504ae35e
Nathan Hüsken <nathan@huesken.org>
2024-09-23 17:16:12 +0200

    redo layzgit activation script to work on android

--- nixos/home-manager/nathan.nix
+++ nixos/home-manager/nathan.nix
@@ -51,7 +51,6 @@ in {
     spectral-language-server
     # pandoc
     unstable.pandoc
-    plantuml-headless
     d2
     nodePackages.mermaid-cli
     # lua
@@ -142,32 +141,36 @@ in {
   # neovim/LazyVim activation script
   home.activation = {
     initLazyVim = lib.hm.dag.entryAfter ["installPackages"] ''
-    export PATH="${config.home.path}/bin:$PATH"
-    if [ ! -d ~/.config/nvim ]; then
-      $DRY_RUN_CMD git clone https://github.com/LazyVim/starter ~/.config/nvim
+    export PATH="$HOME/bin:$PATH"
+    if [ ! -d "$HOME/.config/nvim" ]; then
+      $DRY_RUN_CMD ${pkgs.git}/bin/git clone https://github.com/LazyVim/starter $HOME/.config/nvim
     fi
 
     # symlink the layzvim.json
-    $DRY_RUN_CMD rm -rf ~/.config/nvim/lazyvim.json
-    $DRY_RUN_CMD ln -s ~/dotfiles/lazyvim/lazyvim.json ~/.config/nvim/lazyvim.json
+    $DRY_RUN_CMD rm -rf $HOME/.config/nvim/lazyvim.json
+    $DRY_RUN_CMD ln -s $HOME/dotfiles/lazyvim/lazyvim.json $HOME/.config/nvim/lazyvim.json
 
     # symlink files in config dir
-    for config_file in ~/dotfiles/lazyvim/lua/config/*.lua; do
+    for config_file in $HOME/dotfiles/lazyvim/lua/config/*.lua; do
+      $DRY_RUN_CMD mkdir -p $HOME/.config/nvim/config
       export target_config_file="$HOME/.config/nvim/lua/config/$(basename $config_file)"
       $DRY_RUN_CMD rm -rf "$target_config_file"
+      echo "$config_file -> $target_config_file"
       $DRY_RUN_CMD ln -s "$config_file" "$target_config_file"
     done
 
     # symlink files in plugin dir
-    for plugin_file in ~/dotfiles/lazyvim/lua/plugins/*.lua; do
+    for plugin_file in $HOME/dotfiles/lazyvim/lua/plugins/*.lua; do
+      $DRY_RUN_CMD mkdir -p $HOME/.config/nvim/plugins
       export target_plugin_file="$HOME/.config/nvim/lua/plugins/$(basename $plugin_file)"
       $DRY_RUN_CMD rm -rf "$target_plugin_file"
+      echo "$plugin_file -> $target_plugin_file"
       $DRY_RUN_CMD ln -s "$plugin_file" "$target_plugin_file"
     done
     '';
     zellijConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD rm -rf ~/.config/zellij
-    $DRY_RUN_CMD ln -s ~/dotfiles/zellij ~/.config/zellij
+    $DRY_RUN_CMD rm -rf $HOME/.config/zellij
+    $DRY_RUN_CMD ln -s $HOME/dotfiles/zellij $HOME/.config/zellij
     '';
   };
 }
