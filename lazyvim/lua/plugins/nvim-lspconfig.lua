852cf7d9f5d1293f51b69cc7fa8be5a460a5295a
Nathan Hüsken <nathan@huesken.org>
2024-10-14 13:43:33 +0200

    disable installing certain lsps through mason on arm

--- lazyvim/lua/plugins/nvim-lspconfig.lua
+++ lazyvim/lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,21 @@
+local is_arm = vim.loop.os_uname().machine == "aarch64"
+
+-- dont use mason for somee lspss ion arm
+local servers = {}
+if is_arm then
+  servers["lua_ls"] = { mason = false }
+  servers["erlangls"] = { mason = false }
+  servers["nil_ls"] = { mason = false }
+  servers["helm_ls"] = { mason = false }
+  servers["texlab"] = { mason = false }
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     autoformat = false,
     codelens = { enabled = true },
     inlay_hints = { exclude = { "vue", "lua" } },
+    servers = servers,
   },
 }
