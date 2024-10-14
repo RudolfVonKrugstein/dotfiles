99f2513abd5ac15904d79523f3a04106301ea105
Nathan Hüsken <nathan@huesken.org>
2024-10-14 13:43:07 +0200

    fix autosave plugin, only save on leaving buffer on loosing focus

--- lazyvim/lua/plugins/autosave.lua
+++ lazyvim/lua/plugins/autosave.lua
@@ -2,6 +2,12 @@ return {
   -- plugin: auto-save.nvim
   -- function: auto save changes
   -- src: https://github.com/pocco81/auto-save.nvim
-  "Pocco81/auto-save.nvim",
-  opts = { debounce_delay = 2000 },
+  "okuuva/auto-save.nvim",
+  opts = {
+    trigger_events = {
+      immediate_save = { "BufLeave", "FocusLost" },
+      defer_save = {},
+    },
+    debounce_delay = 2000,
+  },
 }
