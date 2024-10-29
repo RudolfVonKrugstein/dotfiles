0e1686d00fa173fc83b16a9ca20a08ddbdeda1f4
Nathan Hüsken <nathan@huesken>
2024-10-29 15:24:44 +0100

    use latest quarto version

--- nixos/package-overrides.nix
+++ nixos/package-overrides.nix
@@ -2,5 +2,6 @@
 {
   nixpkgs.config.packageOverrides = pkgs: rec {
     plantuml-headless = pkgs.callPackage ./packages/plantuml-headless { };
+    quarto = pkgs.callPackage ./packages/quarto {};
   };
 }
