d76ef389b474964de41794bd3243ae13236f0d4b
Nathan Hüsken <nathan@huesken>
2024-11-01 09:45:25 +0100

    install latest version of d2

--- nixos/package-overrides.nix
+++ nixos/package-overrides.nix
@@ -3,5 +3,6 @@
   nixpkgs.config.packageOverrides = pkgs: rec {
     plantuml-headless = pkgs.callPackage ./packages/plantuml-headless { };
     quarto = pkgs.callPackage ./packages/quarto {};
+    d2 = pkgs.callPackage ./packages/d2 {};
   };
 }
