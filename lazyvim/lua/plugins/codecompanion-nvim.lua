1f77df709f844da689f9602f1f77a6c4f7468d8d
Nathan Hüsken <nathan@huesken>
2024-12-20 10:20:01 +0100

    only activate codecompanion if anthropic key is available

--- lazyvim/lua/plugins/codecompanion-nvim.lua
+++ lazyvim/lua/plugins/codecompanion-nvim.lua
@@ -1,28 +1,36 @@
-return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    default_adapter = "anthropic",
-    adapters = {
-      anthropic = function()
-        return require("codecompanion.adapters").extend("anthropic", {
-          env = {
-            api_key = "cmd:cat /home/nathan/.ANTHROPIC_API_KEY",
-          },
-        })
-      end,
+local function file_exists(name)
+  local f = io.open(name, "r")
+  return f ~= nil and io.close(f)
+end
+
+-- test if antoropic api key exists and enable codecompanion
+if file_exists("/home/nathan/.ANTHROPIC_API_KEY") then
+  return {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
-    strategies = {
-      chat = {
-        adapter = "anthropic",
+    opts = {
+      default_adapter = "anthropic",
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd:cat /home/nathan/.ANTHROPIC_API_KEY",
+            },
+          })
+        end,
       },
-      inline = {
-        adapter = "anthropic",
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "anthropic",
+        },
       },
     },
-  },
-  config = true,
-}
+    config = true,
+  }
+end
