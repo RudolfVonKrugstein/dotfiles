5104310106cb9796e516db10d0ca95cd7287256c
Nathan Hüsken <nathan@huesken>
2025-02-15 11:53:19 +0100

    add zellij plugins

--- nixos/home-manager/zellij-activate.nix
+++ nixos/home-manager/zellij-activate.nix
@@ -8,6 +8,10 @@ pkgs,
       $DRY_RUN_CMD rm -rf $HOME/.config/zellij
       $DRY_RUN_CMD cd $HOME/.config/
       $DRY_RUN_CMD ln -s ../dotfiles/zellij $HOME/.config/zellij
+
+      $DRY_RUN_CMD mkdir -p $HOME/.config/zellij/plugins
+      $DRY_RUN_CMD ${pkgs.wget}/bin/wget https://github.com/laperlej/zellij-sessionizer/releases/latest/download/zellij-sessionizer.wasm -O ~/.config/zellij/plugins/zellij-sessionizer.wasm
+      $DRY_RUN_CMD ${pkgs.wget}/bin/wget https://github.com/strech/zbuffers/releases/latest/download/zbuffers.wasm -O ~/.config/zellij/plugins/zbuffers.wasm
       '';
     };
 }
