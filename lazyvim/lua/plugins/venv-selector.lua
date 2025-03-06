5227f6c8e446d860085ae30d57087ba5fd64a8b9
Nathan Hüsken <nathan.huesken-extern@deutschebahn.com>
2025-03-06 08:56:02 +0100

    fix venv-select when directories are to long

--- lazyvim/lua/plugins/venv-selector.lua
+++ lazyvim/lua/plugins/venv-selector.lua
@@ -1,12 +1,26 @@
+local function shorter_name(filename)
+  if filename:len() > 70 then
+    return filename:sub("..." .. filename:len() - 70)
+  end
+  return filename
+end
+
 return {
   "linux-cultist/venv-selector.nvim",
   dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   cmd = "VenvSelect",
-  opts = {
-    -- Your options go here
-    name = { "venv", ".venv" },
-    auto_refresh = true,
-  },
+  config = function()
+    require("venv-selector").setup({
+      settings = {
+        options = {
+          -- Your options go here
+          name = { "venv", ".venv" },
+          auto_refresh = true,
+          on_telescope_result_callback = shorter_name,
+        },
+      },
+    })
+  end,
   event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
     -- Keymap to open VenvSelector to pick a venv.
