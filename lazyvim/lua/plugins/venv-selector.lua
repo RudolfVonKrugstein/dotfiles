c961f1075be01fc038e595976dab2dad4188f49e
Nathan Hüsken <nathan.huesken-extern@deutschebahn.com>
2025-04-15 08:37:51 +0200

    update venv

--- lazyvim/lua/plugins/venv-selector.lua
+++ lazyvim/lua/plugins/venv-selector.lua
@@ -16,9 +16,19 @@ return {
           -- Your options go here
           name = { "venv", ".venv" },
           auto_refresh = true,
-          on_telescope_result_callback = shorter_name,
         },
       },
+    })
+    vim.api.nvim_create_autocmd("VimEnter", {
+      desc = "Auto select virtualenv Nvim open",
+      pattern = "*",
+      callback = function()
+        local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+        if venv ~= "" then
+          require("venv-selector").retrieve_from_cache()
+        end
+      end,
+      once = true,
     })
   end,
   event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
