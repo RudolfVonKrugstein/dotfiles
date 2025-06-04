3bbfd9c43a6ea047cd25216537c6aa57301836e9
Nathan Hüsken <nathan.huesken-extern@deutschebahn.com>
2025-06-04 08:42:59 +0200

    enable codecompanion

--- lazyvim/lua/plugins/codecompanion-nvim.lua
+++ lazyvim/lua/plugins/codecompanion-nvim.lua
@@ -1,65 +1,65 @@
-return {}
+local function file_exists(name)
+  local f = io.open(name, "r")
+  return f ~= nil and io.close(f)
+end
 
--- local function file_exists(name)
---   local f = io.open(name, "r")
---   return f ~= nil and io.close(f)
--- end
---
--- local adapters = {
---   adapters = {
---     ollama = function()
---       return require("codecompanion.adapters").extend("ollama", {
---         schema = {
---           model = {
---             default = "qwen2.5-coder:7b-instruct",
---           },
---         },
---         env = {
---           url = "http://localhost:11434",
---         },
---         headers = {
---           ["Content-Type"] = "application/json",
---         },
---         parameters = {
---           sync = true,
---         },
---       })
---     end,
---   },
--- }
--- if file_exists("/home/nathan/.ANTHROPIC_API_KEY") then
---   adapters["anthropic"] = function()
---     return require("codecompanion.adapters").extend("anthropic", {
---       env = {
---         api_key = "cmd:cat /home/nathan/.ANTHROPIC_API_KEY",
---       },
---     })
---   end
--- end
---
--- -- test if antoropic api key exists and enable codecompanion
--- return {
---   "olimorris/codecompanion.nvim",
---   dependencies = {
---     "nvim-lua/plenary.nvim",
---     "nvim-treesitter/nvim-treesitter",
---   },
---   opts = {
---     default_adapter = "anthoropic",
---     display = {
---       diff = {
---         provider = "mini_diff",
---       },
---     },
---     adapters = adapters,
---     strategies = {
---       chat = {
---         adapter = "anthropic",
---       },
---       inline = {
---         adapter = "anthropic",
---       },
---     },
---   },
---   config = true,
--- }
+local adapters = {
+  adapters = {
+    ollama = function()
+      return require("codecompanion.adapters").extend("ollama", {
+        schema = {
+          model = {
+            default = "qwen2.5-coder:7b-instruct",
+          },
+        },
+        env = {
+          url = "http://localhost:11434",
+        },
+        headers = {
+          ["Content-Type"] = "application/json",
+        },
+        parameters = {
+          sync = true,
+        },
+      })
+    end,
+  },
+}
+local default_adapter = "copilot"
+if file_exists("/home/nathan/.ANTHROPIC_API_KEY") then
+  default_adapter = "anthropic"
+  adapters["anthropic"] = function()
+    return require("codecompanion.adapters").extend("anthropic", {
+      env = {
+        api_key = "cmd:cat /home/nathan/.ANTHROPIC_API_KEY",
+      },
+    })
+  end
+end
+
+-- test if antoropic api key exists and enable codecompanion
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    default_adapter = "anthoropic",
+    display = {
+      diff = {
+        provider = "mini_diff",
+      },
+    },
+    adapters = adapters,
+    strategies = {
+      chat = {
+        adapter = default_adapter,
+      },
+      inline = {
+        adapter = default_adapter,
+      },
+    },
+  },
+  config = true,
+}
