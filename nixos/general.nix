9166f95f2c4e84f2efc42b3000217b572735cbad
Nathan Hüsken <nathan@huesken>
2025-08-11 11:22:05 +0200

    enable nix-command and flakes

--- nixos/general.nix
+++ nixos/general.nix
@@ -1,5 +1,7 @@
 {config,lib,pkgs, ...}:
 {
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Set your time zone
   time.timeZone = "Europe/Berlin";
 
