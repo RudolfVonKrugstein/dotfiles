f7bc50fcb6ee780a84add95573ce89786df03ebe
Nathan Hüsken <nathan@huesken>
2025-09-05 12:01:54 +0200

    add diagnostic preview shortcut

--- nvim/lua/options/diagnostics.lua
+++ nvim/lua/options/diagnostics.lua
@@ -4,18 +4,27 @@ vim.keymap.set("n", "]q", "<cmd>cnext<CR>zz", { desc = "next item in quickfix" }
 vim.keymap.set("n", "[q", "<cmd>cprev<CR>zz", { desc = "prev item in quickfix" })
 
 vim.keymap.set("n", "]w", function()
-  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN, wrap = true })
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN, wrap = true, float = true })
 end, { desc = "next warning" })
 vim.keymap.set("n", "[w", function()
-  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN, wrap = true })
+  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN, wrap = true, float = true })
 end, { desc = "prev warning" })
 
 vim.keymap.set("n", "]e", function()
-  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR, wrap = true })
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR, wrap = true, float = true })
 end, { desc = "next error" })
 vim.keymap.set("n", "[e", function()
-  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR, wrap = true })
+  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR, wrap = true, float = true })
 end, { desc = "prev error" })
+
+vim.keymap.set("n", "<leader>dd", function()
+  local opts = {
+    focusable = false,
+    scope = "cursor",
+    close_events = { "BufLeave", "CursorMoved", "InsertEnter" },
+  }
+  vim.diagnostic.open_float(nil, opts)
+end)
 
 -- diagnostics setup
 vim.diagnostic.config({
