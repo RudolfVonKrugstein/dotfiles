72d599f99e07094757b15ac71af80e332f43747e
Nathan Hüsken <nathan@huesken.org>
2026-04-10 21:51:02 +0200

    update statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -10,8 +10,54 @@ local config = {
     path_hidden = "",
     branch_hidden = "",
   },
+  git_add_hl = "StatusGitAdd",
+  git_delete_hl = "StatusGitDelete",
+  git_change_hl = "StatusGitChange",
+
+  hint_hl = "DiagnosticHint",
+  info_hl = "DiagnosticInfo",
+  warning_hl = "DiagnosticWarn",
+  error_hl = "DiagnosticError",
+
   placeholder_hl = "StatusLineDim",
+  normal_hl = "StatusNormal",
+  command_hl = "StatusCommand",
+  terminal_hl = "StatusTerminal",
+  insert_hl = "StatusInsert",
+  replace_hl = "StatusReplace",
+  visual_line_hl = "StatusVisualLine",
+  visual_block_hl = "StatusVisualBlock",
+  replacep_pending_hl = "StatusReplacePending",
+  visual_hl = "StatusVisual",
 }
+
+-- Mode icons
+local mode_icons = {
+  n = { text = " N ", hl = config.normal_hl },
+  c = { text = " C ", hl = config.command_hl },
+  t = { text = " T ", hl = config.terminal_hl },
+  i = { text = " I ", hl = config.insert_hl },
+  R = { text = " R ", hl = config.replace_hl },
+  V = { text = "V-L", hl = config.visual_line_hl },
+  [""] = { text = "V-B", hl = config.visual_block_hl }, -- Visual Block
+  r = { text = "R-P", hl = config.replacep_pending_hl },
+  v = { text = " V ", hl = config.visual_hl },
+}
+
+local colors = require("tokyonight.colors").setup()
+vim.api.nvim_set_hl(0, config.git_add_hl, { fg = colors.green }) -- create if missing
+vim.api.nvim_set_hl(0, config.git_change_hl, { fg = colors.yellow }) -- create if missing
+vim.api.nvim_set_hl(0, config.git_delete_hl, { fg = colors.red }) -- create if missing
+
+vim.api.nvim_set_hl(0, config.normal_hl, { fg = colors.fg_light, bg = colors.blue7 }) -- create if missing
+vim.api.nvim_set_hl(0, config.command_hl, { fg = colors.fg_dark, bg = colors.purple }) -- create if missing
+vim.api.nvim_set_hl(0, config.terminal_hl, { fg = colors.fg_dark, bg = colors.green2 }) -- create if missing
+vim.api.nvim_set_hl(0, config.insert_hl, { fg = colors.fg_dark, bg = colors.green }) -- create if missing
+vim.api.nvim_set_hl(0, config.replace_hl, { fg = colors.fg_dark, bg = colors.red1 }) -- create if missing
+vim.api.nvim_set_hl(0, config.visual_line_hl, { fg = colors.fg_dark, bg = colors.yellow }) -- create if missing
+vim.api.nvim_set_hl(0, config.visual_block_hl, { fg = colors.fg_dark, bg = colors.yellow }) -- create if missing
+vim.api.nvim_set_hl(0, config.replacep_pending_hl, { fg = colors.fg_dark, bg = colors.red2 }) -- create if missing
+vim.api.nvim_set_hl(0, config.visual_hl, { fg = colors.fg_dark, bg = colors.orange }) -- create if missing
 
 -- helper to wrap text in a statusline highlight group
 local function hl(group, text)
@@ -21,6 +67,11 @@ end
 -- create and link the highlight group(s)
 vim.api.nvim_set_hl(0, config.placeholder_hl, {}) -- create if missing
 vim.api.nvim_set_hl(0, config.placeholder_hl, { link = "Comment" })
+
+local function mode()
+  local m = vim.fn.mode()
+  return hl(mode_icons[m]["hl"], mode_icons[m]["text"])
+end
 
 local function filepath()
   local fpath = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.:h")
@@ -63,23 +114,76 @@ local function git()
   return table.concat({
     "[ ",
     head,
-    added,
-    changed,
-    removed,
+    hl(config.git_add_hl,added),
+    hl(config.git_change_hl,changed),
+    hl(config.git_delete_hl,removed),
     "]",
   })
+end
+
+-- Diagnostics symbols
+local function get_diagnostics()
+	if not vim.diagnostic then
+		return ""
+	end
+	local d = vim.diagnostic.get(0)
+	local e, w, i, h = 0, 0, 0, 0
+	for _, v in ipairs(d) do
+		if v.severity == vim.diagnostic.severity.ERROR then
+			e = e + 1
+		elseif v.severity == vim.diagnostic.severity.WARN then
+			w = w + 1
+		elseif v.severity == vim.diagnostic.severity.INFO then
+			i = i + 1
+		elseif v.severity == vim.diagnostic.severity.HINT then
+			h = h + 1
+		end
+	end
+
+	local s = ""
+	if e > 0 then
+		s = s .. hl(config.error_hl," " .. e .. " ")
+	end
+	if w > 0 then
+		s = s .. hl(config.warning_hl," " .. w .. " ")
+	end
+	if i > 0 then
+		s = s .. hl(config.info_hl," " .. i .. " ")
+	end
+	if h > 0 then
+		s = s .. hl(config.hint_hl," " .. h .. " ")
+	end
+
+	-- reset to StatusLine for following text
+	return s .. "%#StatusLine#"
+end
+
+local function lsps()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local active_lsps = vim.lsp.get_clients({ bufnr = bufnr })
+
+  local names = {}
+  for _, lsp in ipairs(active_lsps) do
+    table.insert(names, lsp["name"])
+  end
+
+  return table.concat(names, "|")
 end
 
 Statusline = {}
 
 function Statusline.active()
   return table.concat({
-    "[",
+    mode(),
+    " [",
     filepath(),
     "%t] ",
     git(),
-    "%=",
-    "%y [%P %l:%c]",
+    "%=[",
+    lsps(),
+    "]%< ",
+    get_diagnostics(),
+    " %y [%P %l:%c]",
   })
 end
 
