07fa561000727753577aeac4a5671f6676b32a8d
Nathan Hüsken <nathan@huesken.org>
2026-04-10 18:35:12 +0200

    style comments and unused code diffently

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,13 @@
 vim.pack.add({ "https://github.com/folke/tokyonight.nvim" })
 require("tokyonight").setup({
-  styles = {
-    comments = { italic = false }, -- Disable italics in comments
-  },
+  on_highlights = function(hl, colors)
+    hl.Comments = {
+      fg = "#737aa2", -- brighter than default, still palette-native
+      italic = false,
+    }
+    hl.DiagnosticUnnecessary = {
+      fg = colors.red1,
+    }
+  end,
 })
 vim.cmd.colorscheme("tokyonight-night")
