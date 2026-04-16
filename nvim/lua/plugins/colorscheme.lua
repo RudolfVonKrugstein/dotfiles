6792bbe01f605a25ede3e299749580bc1a262e57
Nathan Hüsken <nathan@huesken.org>
2026-04-16 15:31:47 +0200

    better colros

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 vim.pack.add({ "https://github.com/folke/tokyonight.nvim" })
+local util = require("tokyonight.util")
 require("tokyonight").setup({
   on_highlights = function(hl, colors)
-    hl.Comments = {
-      fg = "#737aa2", -- brighter than default, still palette-native
+    hl.Comment = {
+      fg = util.lighten(colors.comment, 0.3), -- brighter than default, still palette-native
       italic = false,
     }
     hl.DiagnosticUnnecessary = {
-      fg = colors.red1,
+      bg = "#300000",
+      underline = false,
     }
   end,
 })
