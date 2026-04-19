72f16ae3121e013a9b4cd0a84776316cd7027a20
Nathan Hüsken <nathan@huesken.org>
2026-04-19 19:37:39 +0200

    install recall

---
+++ nvim/lua/plugins/recall.lua
@@ -0,0 +1,16 @@
+vim.pack.add({ "https://github.com/fnune/recall.nvim" })
+
+require("recall").setup({})
+vim.keymap.set("n", "<leader>ml", require("recall.snacks").pick, { desc = "List marks" })
+vim.keymap.set("n", "<leader>mm", "<cmd>RecallToggle<CR>", { desc = "Toggle mark" })
+vim.keymap.set("n", "]r", "<cmd>RecallNext<CR>", { desc = "Next mark" })
+vim.keymap.set("n", "[r", "<cmd>RecallPrev<CR>", { desc = "Prev mark" })
+vim.keymap.set("n", "<leader>mc", function()
+  local choice = vim.fn.confirm("Really remove all marks?", "&Yes\n&No", 2)
+  if choice == 1 then
+    vim.cmd("RecallClear")
+    print("all marks removed.")
+  else
+    print("doing nothing.")
+  end
+end, { desc = "Delete all marks" })
