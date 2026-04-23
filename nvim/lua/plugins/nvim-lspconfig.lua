3d8dcb9888a541195ed93c3d95293917076a9acb
Nathan Hüsken <nathan@huesken.org>
2026-04-23 14:26:37 +0200

    install lsp-toggle

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,1 +1,4 @@
-vim.pack.add({"https://github.com/neovim/nvim-lspconfig" })
+vim.pack.add({ "https://github.com/neovim/nvim-lspconfig", "https://github.com/adoyle-h/lsp-toggle.nvim" })
+require("lsp-toggle").setup({ telescope = false })
+
+vim.keymap.set("n", "<leader>cl", "<cmd>ToggleLSP<CR>", { desc = "Toggle LSPs" })
